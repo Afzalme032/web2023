18a4ec3c65ca530de50c95ece1f7c8ffb86cad9a
Afzal <134323612+Afzalme032@users.noreply.github.com>
2024-01-04 00:44:11 +0530

    Add files via upload

---
+++ Profile/script.js
@@ -0,0 +1,6 @@
+let close = document.getElementById('close');
+close.addEventListener('click',()=>{
+    //console.log(close.parentElement.parentElement)
+    close.parentElement.parentElement.style.display = "none"
+   
+})
